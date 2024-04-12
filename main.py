185d7df27199b5af6cf4247e60aa1f5b679c21e9
rodrib <rodribogado50@gmail.com>
2024-04-12 18:30:52 -0300

    update main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
-import pandas as pd
+
 import numpy as np
 
 
