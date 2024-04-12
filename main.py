3eec094a981b6e5a85ede9d26e0125e64f8c8b95
rodrib <rodribogado50@gmail.com>
2024-04-12 18:17:24 -0300

    import numpy because i am idiot and fixed bug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import pandas as pd
-
+import numpy as np
 
 
 app = FastAPI()
